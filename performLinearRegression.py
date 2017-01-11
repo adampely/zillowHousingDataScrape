99373074521d2ca5c6c4a5514416282c9c7c8d86
adampely <abp19@duke.edu>
2017-01-11 10:42:18 -0500

    added some visualization for price vs. sqft and price vs. rooms

--- performLinearRegression.py
+++ performLinearRegression.py
@@ -1,13 +1,33 @@
 import pandas as pd
-import numpy as np
+import numpy
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
 
 #def performLinearRegression(dataCsvFile = 'allDurhamData.csv'):
 house_data = pd.read_csv('allDurhamData.csv', index_col=0, thousands=',')
+#house_data_sql = house_data.to_sql('house_data')
 X = house_data[['bedrooms', 'bathrooms', 'sqft']]
 house_price = house_data['zestimate']
 house_rent = house_data['real rent']
 house_price_rent_ratio = house_rent / house_price
+sortedByBedrooms = [];
+numBedrooms = [];
+for i in sorted(house_data.bedrooms.unique()):
+    numBedrooms.append(i)
+    valList = numpy.array(list(house_data.zestimate[list(house_data.bedrooms == i)]))
+    valList = valList[~numpy.isnan(valList)]
+    sortedByBedrooms.append(valList)
+
+    
+plt.subplot(3,1,1)
+plt.plot(X['sqft'], house_price, 'ro')
+plt.subplot(3,1,2)
+plt.boxplot(sortedByBedrooms)
+#plt.xticks(numBedrooms)
+plt.subplot(3,1,3)
+#plt.boxplot(X['bathrooms'], house_price)
+plt.show()
 print(house_data.head())
 
 #normalize the data
@@ -18,3 +38,5 @@ for column_name in X.keys():
 Xnorm = house_data[['bedrooms_norm', 'bathrooms_norm', 'sqft_norm']]
 
 est = sm.OLS(house_price, Xnorm).fit()
+
+
