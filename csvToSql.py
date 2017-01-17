a93a04d1180628ac5944e402be2443a1ae6ad1cd
adampely <abp19@duke.edu>
2017-01-17 10:18:13 -0500

    created a where.js output file which allows for the mapping of the best rent:price properties on a google map

--- csvToSql.py
+++ csvToSql.py
@@ -3,9 +3,9 @@ import sqlite3
 import os
 
 #csvNamesList = ['houses1.csv', 'houses2.csv', 'houses3.csv']
-def csvToSql(databaseName = 'housingData.sqlite', csvNamesList = ['allDurhamData.csv']):
+def csvToSql(databaseName = 'housingDataWithCoords.sqlite', csvNamesList = ['houses1.csv', 'houses2.csv', 'houses3.csv', 'houses4.csv', 'houses5.csv', 'houses6.csv', 'houses7.csv', 'houses8.csv']):
     if not os.path.isfile(databaseName):
-        conn = sqlite3.connect('housingData.sqlite')
+        conn = sqlite3.connect(databaseName)
         for csvName in csvNamesList:
             house_data = pd.read_csv(csvName, index_col=0, thousands=',')
             house_data.to_sql('houses', conn, if_exists='append', index=False)
